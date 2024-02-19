aed279df6af4aebe314ed553a9331dbf2df8aa63
ahmed-my <yahaya.mymohammed@gmail.com>
2024-02-19 17:08:29 +0000

    API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Script takes an employee ID as a command-line argument and fetches
+the corresponding user information and to-do list from the JSONPlaceholder API.
+then prints the tasks completed by the employee.
+"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    employee_id = sys.argv[1]
+    user_response = requests.get(url + "users/{}".format(employee_id))
+    user = user_response.json()
+    resource = {"userId": employee_id}
+    todos_request = requests.get(url + "todos", resource=resource)
+    todos = todos_request.json()
+    completed = []
+    for todo in todos:
+        if todo.get("completed") is True:
+            completed.append(todo.get("title"))
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+
+    for complete in completed:
+        print("\t {}".format(complete))
---
+++ 0x15-api/README.md
@@ -0,0 +1,2 @@
+#0x15 API
+Application Program Interface
