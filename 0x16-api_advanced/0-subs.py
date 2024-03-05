993f89315ae7d6cf852110e74459468f9b899843
ahmed-my <yahaya.mymohammed@gmail.com>
2024-03-05 12:37:44 +0000

    API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ import requests
 def number_of_subscribers(subreddit):
     """returns the total number of subscribers for a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Chrome/122.0.0.0"}
+    headers = {"User-Agent": "Mozilla/5.0"}
     res = requests.get(url, headers=headers, allow_redirects=False)
     if res.status_code == 200:
         data = res.json()
