2b8037049c459f4a99eb93cab3401df905b99815
ahmed-my <yahaya.mymohammed@gmail.com>
2024-03-05 17:35:53 +0000

    web dynamic

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,8 +3,6 @@
 Script that queries subscribers on a given Reddit subreddit.
 """
 import requests
-import sys
-
 def number_of_subscribers(subreddit):
     """returns the total number of subscribers for a given subreddit."""
 
