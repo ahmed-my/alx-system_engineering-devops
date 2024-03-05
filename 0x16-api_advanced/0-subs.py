5010eb880bea7a081e35d23c87a02981d56c7ff9
ahmed-my <yahaya.mymohammed@gmail.com>
2024-03-05 13:31:37 +0000

    API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,12 +2,12 @@
 """
 Script that queries subscribers on a given Reddit subreddit.
 """
-
 import requests
 
 
 def number_of_subscribers(subreddit):
     """returns the total number of subscribers for a given subreddit."""
+
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
