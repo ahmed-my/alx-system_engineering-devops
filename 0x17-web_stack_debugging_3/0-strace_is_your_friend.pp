0373227fa346bc34ee14447f5141a1d4da1e3f84
ahmed-my <yahaya.mymohammed@gmail.com>
2024-03-08 20:27:54 +0000

    API advanced

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,5 +2,6 @@
 
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  onlyif  => "test -f /var/www/html/wp-settings.php",
   path    => '/usr/local/bin/:/bin/'
 }
